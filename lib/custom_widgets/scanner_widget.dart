b47cdc042175d5c319930b62c2582f0393e9468d
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-12-04 00:37:02 +0100

    Scanner Modes starting point
    Now only the actual implementation is left...
    Waiting for more API features for that:
    https://github.com/Unrealitix/UIMS.Api/issues/8

--- lib/custom_widgets/scanner_widget.dart
+++ lib/custom_widgets/scanner_widget.dart
@@ -1,8 +1,16 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../models/inventory_item.dart';
+import '../utils.dart';
+
+enum _ScannerMode {
+  newItem,
+  addition,
+  subtraction,
+}
 
 class ManagedScannerWidget extends StatefulWidget {
   const ManagedScannerWidget({super.key});
@@ -27,6 +35,8 @@ class _ManagedScannerWidgetState extends State<ManagedScannerWidget> {
   bool isPopupCurrentlyOpen = false;
   String lastScannedCode = "";
 
+  _ScannerMode scannerMode = _ScannerMode.newItem;
+
   final AudioPlayer player = AudioPlayer();
   final Source newBeep = AssetSource("sounds/Scan_new_item.mp3");
   final Source existingBeep = AssetSource("sounds/Scan_existing_item.mp3");
@@ -45,6 +55,18 @@ class _ManagedScannerWidgetState extends State<ManagedScannerWidget> {
     super.dispose();
   }
 
+  //TODO: These are too long. They shouldn't be longer than 30 characters
+  String get _scannerModeString {
+    switch (scannerMode) {
+      case _ScannerMode.newItem:
+        return "Hover over a barcode to add it";
+      case _ScannerMode.addition:
+        return "Hover over a barcode to add to it";
+      case _ScannerMode.subtraction:
+        return "Hover over a barcode to subtract from it";
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Stack(
         children: [
@@ -58,11 +80,35 @@ class _ManagedScannerWidgetState extends State<ManagedScannerWidget> {
               if (text == lastScannedCode) return;
               print("scanned text: $text");
 
-              player.play(newBeep);
-              //TODO: Implement existing item detection
-              // player.play(existingBeep);
+              lastScannedCode = text;
+              isPopupCurrentlyOpen = true;
 
-              _dialogNewScannedItem(context, text);
+              switch (scannerMode) {
+                case _ScannerMode.newItem:
+                  player.play(newBeep);
+                  //TODO: Implement existing item detection:
+                  // if(item is already in inventory) {
+                  //   player.play(existingBeep);
+                  //   dialogEditItem(item);
+                  //   return;
+                  // }
+
+                  _dialogNewScannedItem(context, text);
+                  break;
+                case _ScannerMode.addition:
+                  player.play(existingBeep);
+                  showSnackbar("Addition mode not implemented yet");
+                  //TODO: Implement addition mode
+                  break;
+                case _ScannerMode.subtraction:
+                  player.play(existingBeep);
+                  showSnackbar("Subtraction mode not implemented yet");
+                  //TODO: Implement subtraction mode
+                  break;
+              }
+
+              isPopupCurrentlyOpen = false;
+              _lastScannedCodeDelayedReset();
             },
             onStart: (MobileScannerArguments? arguments) {
               print("CameraComponent::onStart");
@@ -75,13 +121,12 @@ class _ManagedScannerWidgetState extends State<ManagedScannerWidget> {
           ),
           if (hasTorch)
             Align(
-              alignment: Alignment.bottomRight,
+              alignment: Alignment.bottomLeft,
               child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    scannerController.toggleTorch();
-                  });
-                },
+                tooltip: "Toggle flashlight",
+                onPressed: () => setState(() {
+                  scannerController.toggleTorch();
+                }),
                 icon: Icon(
                   size: 32,
                   color: Colors.white,
@@ -97,21 +142,49 @@ class _ManagedScannerWidgetState extends State<ManagedScannerWidget> {
                 ),
               ),
             ),
-          const Align(
+          Align(
             alignment: Alignment.bottomCenter,
-            child: Chip(label: Text("Hover over a barcode to scan it")),
+            child: Chip(label: Text(_scannerModeString)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: SpeedDial(
+                tooltip: "Add item",
+                icon: Icons.party_mode,
+                activeIcon: Icons.close,
+                children: [
+                  SpeedDialChild(
+                    child: const Icon(Icons.fiber_new_outlined),
+                    label: "Add new item",
+                    onTap: () => setState(() {
+                      scannerMode = _ScannerMode.newItem;
+                    }),
+                  ),
+                  SpeedDialChild(
+                    child: const Icon(Icons.add),
+                    label: "Add quantity",
+                    onTap: () => setState(() {
+                      scannerMode = _ScannerMode.addition;
+                    }),
+                  ),
+                  SpeedDialChild(
+                    child: const Icon(Icons.remove),
+                    label: "Subtract quantity",
+                    onTap: () => setState(() {
+                      scannerMode = _ScannerMode.subtraction;
+                    }),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       );
 
   _dialogNewScannedItem(BuildContext context, String text) async {
-    lastScannedCode = text;
-    isPopupCurrentlyOpen = true;
-
     Item? item = await Item.dialogNewItem(context, barcode: text);
-
-    isPopupCurrentlyOpen = false;
-    _lastScannedCodeDelayedReset();
 
     if (item == null) {
       print("item was null");
